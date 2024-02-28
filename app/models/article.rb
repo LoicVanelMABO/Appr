66fff88d7250699aa5f86d8a83b8bef242cd8aeb
Loic Vanel MABOMESI <loicvanelmabomesinkanko@gmail.com>
2024-02-28 16:22:50 +0100

    2 turn

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    has_many : comments
+    validates :title, presence: true
+    validates :body, presence:true, length:{minimum: 10}
+    validates :price, presence:true
+    validates :devise_id, presence:true
+end
